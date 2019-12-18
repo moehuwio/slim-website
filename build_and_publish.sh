fb906c59359e5bc351a4ed20a6b5a211bede56b8
Michael McCann <michael.thompson.mccann@gmail.com>
2019-12-17 22:00:42 -0700

    Get deployment online working

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,4 +1,8 @@
+jekyll build .
 echo Enter your MSU EGR username:
 read username
-scp -r * $username@scully.egr.msu.edu:/egr/sites/user/html/slimgroup/web
+ssh $username@scully.egr.msu.edu rm -rf '/egr/sites/user/html/slimgroup/web/*'
+scp -r _site/* $username@scully.egr.msu.edu:/egr/sites/user/html/slimgroup/web/
+ssh $username@scully.egr.msu.edu chmod -R a+rX '/egr/sites/user/html/slimgroup/web/'
+read -p "DONE"
 
